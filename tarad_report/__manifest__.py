bc0854c4bc8c5fc54ee6b15b0fabb6cd96bfe478
kongrattapong <rattapong.chok@gmail.com>
2019-09-10 14:16:25 +0700

    [12.0][ADD] Tarad Report

---
+++ tarad_report/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2019 Ecosoft Co., Ltd (https://ecosoft.co.th/)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html)
+
+{
+    "name": "Tarad Report",
+    "summary": "",
+    "version": "12.0.1.0.0",
+    "author": "Ecosoft,Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "website": "https://github.com/OCA/contract",
+    'category': 'Agreement',
+    "depends": [
+        'agreement_contract',
+    ],
+    "data": [
+        "report/contract.xml",
+        "report/contract_view.xml",
+    ],
+}
