f3c07d72c7beae32e91fabb20ca1e99a772c1d8e
kongrattapong <rattapong.chok@gmail.com>
2019-11-16 03:24:05 +0700

    [12.0][FIX] acm_batch_invoice 15/11

--- acm_batch_invoice/models/account_invoice.py
+++ acm_batch_invoice/models/account_invoice.py
@@ -11,7 +11,35 @@ class AccountInvoice(models.Model):
     batch_invoice_id = fields.Many2one(
         comodel_name="acm.batch.invoice",
         string="Batch Invoice",
+        readonly=True,
     )
+
+    @api.multi
+    def remove_menu_print(self, res, reports):
+        # Remove reports menu
+        for report in reports:
+            reports = self.env.ref(report, raise_if_not_found=False)
+            for rec in res.get('toolbar', {}).get('print', []):
+                if rec.get('id', False) in reports.ids:
+                    del res['toolbar']['print'][
+                        res.get('toolbar', {}).get('print').index(rec)]
+        return res
+
+    @api.model
+    def fields_view_get(self, view_id=None, view_type='form',
+                        toolbar=False, submenu=False):
+        hide_reports_base = [
+            'acm_batch_invoice.action_report_batch_invoice',
+        ]
+        type = self._context.get('type')
+        res = super(AccountInvoice, self).fields_view_get(
+            view_id=view_id, view_type=view_type,
+            toolbar=toolbar, submenu=submenu)
+        if res and view_type in ['tree', 'form']:
+            # del menu report customer invoice
+            if type and type != 'out_invoice':
+                self.remove_menu_print(res, hide_reports_base)
+        return res
 
     @api.multi
     def amount_text(self, amount):
