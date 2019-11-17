9eceaea732494a647d5b1d77f89eb0ca2d7010d3
kongrattapong <rattapong.chok@gmail.com>
2019-11-17 18:33:09 +0700

    invoice report

---
+++ acm_rental_collect/models/account_invoice.py
@@ -0,0 +1,23 @@
+# Copyright 2019 Ecosoft Co., Ltd (https://ecosoft.co.th/)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html)
+
+from odoo import models, fields, api
+
+
+class AccountInvoice(models.Model):
+    _inherit = 'account.invoice'
+    _description = 'Account Invoice'
+
+    move_line_ids = fields.Many2many(
+        comodel_name='account.move.line',
+        string='Move Lines',
+        compute='_compute_move_line_ids',
+    )
+
+    @api.multi
+    def _compute_move_line_ids(self):
+        for rec in self:
+            payment_infos = rec._get_payments_vals()
+            move_ids = list(map(lambda d: d['move_id'], payment_infos))
+            move = rec.env['account.move'].browse(move_ids)
+            rec.move_line_ids = move.mapped('line_ids').sorted('debit', True)
