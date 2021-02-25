355bfb90ddbff11ee46ff13d37dde40b4b477711
newtratip <tharathip.chaweewongphan@gmail.com>
2021-02-25 12:11:26 +0700

    [12.0][ADD] Add param ignore_model_read_only_access_group

--- generic_read_only_user_app/__manifest__.py
+++ generic_read_only_user_app/__manifest__.py
@@ -1,13 +1,13 @@
 # -*- coding: utf-8 -*-
 
 {
-    'name': 'Generic Read Only User Access',
+    "name": "Generic Read Only User Access",
     "author": "Edge Technologies",
-    'version': '12.0.1.0',
-    'category': 'Extra Tools',
-    'summary': " Generic Read only user access for particular login user.",
-    'description': """ This app provides a functionality to make generic user access read only for a particular login user Set user read only
-	restriction on user level. stop user access from the system .user restriction user read only restriction. Restricated user access. limited user access limited. security restriction on user level. user security restriction. 
+    "version": "12.0.1.0",
+    "category": "Extra Tools",
+    "summary": " Generic Read only user access for particular login user.",
+    "description": """ This app provides a functionality to make generic user access read only for a particular login user Set user read only
+	restriction on user level. stop user access from the system .user restriction user read only restriction. Restricated user access. limited user access limited. security restriction on user level. user security restriction.
 
 limited access rights to user
 user limited access
@@ -20,7 +20,7 @@ user read only access
 login user read only access
 login user limited access
 login user restricted access
-user access restriction 
+user access restriction
 login user access restriction
 user security access restriction
 user security restriction
@@ -32,16 +32,16 @@ limited customer access
 
 
     """,
-    "license" : "OPL-1",
-    'live_test_url': "https://youtu.be/J2k28TUCbZo",
-    "images":['static/description/main_screenshot.png'],
-    'depends': ['base','sale_management'],
-    'data': [
-            'security/user_read_only_group.xml',
-            'security/ir.model.access.csv',
-            'views/res_user_read_only.xml',
-            ],
-    'installable': True,
-    'auto_install': False,
-
+    "license": "OPL-1",
+    "live_test_url": "https://youtu.be/J2k28TUCbZo",
+    "images": ["static/description/main_screenshot.png"],
+    "depends": ["base", "sale_management"],
+    "data": [
+        "data/ir_config_parameter_data.xml",
+        "security/user_read_only_group.xml",
+        "security/ir.model.access.csv",
+        "views/res_user_read_only.xml",
+    ],
+    "installable": True,
+    "auto_install": False,
 }
--- generic_read_only_user_app/models/res_user_read_only.py
+++ generic_read_only_user_app/models/res_user_read_only.py
@@ -1,52 +1,98 @@
 # -*- coding: utf-8 -*-
 
-from odoo import api, fields, models, tools,_
+from odoo import api, fields, models, tools, _
 from odoo.exceptions import Warning
 
+
+def _get_ignore_models(self):
+    self._cr.execute(
+        """
+        select key, value
+        from ir_config_parameter
+        where key = 'ignore_model_read_only_access_group'
+    """
+    )
+    param_dict = self._cr.dictfetchall()
+    ignore_models = []
+    if param_dict:
+        try:
+            ignore_models = eval(param_dict[0]["value"])
+        except Exception:
+            ignore_models = []
+    if not isinstance(ignore_models, list):
+        ignore_models = []
+    return ignore_models
+
+
 class ResUser(models.Model):
-    _inherit = 'res.users'
+    _inherit = "res.users"
 
     read_only = fields.Boolean(string="Make Read Only")
-    
-    @api.onchange('read_only')
+
+    @api.onchange("read_only")
     def set_read_only_user(self):
-        read_only_grp_id = self.env['ir.model.data'].get_object_reference('generic_read_only_user_app', 'group_read_only_user')[1]
+        read_only_grp_id = self.env["ir.model.data"].get_object_reference(
+            "generic_read_only_user_app", "group_read_only_user"
+        )[1]
         if not self.read_only:
             self.read_only = True
             group_list = []
             for group in self.groups_id:
                 group_list.append(group.id)
             group_list.append(read_only_grp_id)
-            result= self.write({'groups_id':([(6,0,group_list)])})
-        
+            result = self.write({"groups_id": ([(6, 0, group_list)])})
+
         elif self.read_only:
             self.read_only = False
             group_list2 = []
             for group in self.groups_id:
-                if group.id != read_only_grp_id: 
+                if group.id != read_only_grp_id:
                     group_list2.append(group.id)
-            result= self.write({'groups_id':([(6,0,group_list2)])})
+            result = self.write({"groups_id": ([(6, 0, group_list2)])})
+
 
 class IrModelAccess(models.Model):
-    _inherit = 'ir.model.access'
+    _inherit = "ir.model.access"
 
     @api.model
-    @tools.ormcache_context('self._uid', 'model', 'mode', 'raise_exception', keys=('lang',))
-    def check(self, model, mode='read', raise_exception=True):
-        result = super(IrModelAccess, self).check(model, mode, raise_exception=raise_exception)
-        if self.env.user.has_group('generic_read_only_user_app.group_read_only_user'):
-            if mode != 'read':
-                return False
+    @tools.ormcache_context(
+        "self._uid", "model", "mode", "raise_exception", keys=("lang",)
+    )
+    def check(self, model, mode="read", raise_exception=True):
+        result = super(IrModelAccess, self).check(
+            model, mode, raise_exception=raise_exception
+        )
+        ignore_models = _get_ignore_models(self)
+        if model not in ignore_models:
+            if self.env.user.has_group(
+                "generic_read_only_user_app.group_read_only_user"
+            ):
+                if mode != "read":
+                    return False
         return result
 
+
 class IrRule(models.Model):
-    _inherit = 'ir.rule'
+    _inherit = "ir.rule"
 
     def _compute_domain(self, model_name, mode="read"):
-        res = super(IrRule,self)._compute_domain(model_name, mode)
-        obj_list=['res.users.log','res.users','mail.channel','mail.alias','bus.presence','res.lang']
-        if model_name not in obj_list:
-            if self.env.user.has_group('generic_read_only_user_app.group_read_only_user'):
-                if mode != 'read':
-                    raise Warning(_('Read only user can not done this operation..! (%s)') % self.env.user.name)
+        res = super(IrRule, self)._compute_domain(model_name, mode)
+        obj_list = [
+            "res.users.log",
+            "res.users",
+            "mail.channel",
+            "mail.alias",
+            "bus.presence",
+            "res.lang",
+        ]
+        ignore_models = _get_ignore_models(self)
+        if model_name not in obj_list + ignore_models:
+            if self.env.user.has_group(
+                "generic_read_only_user_app.group_read_only_user"
+            ):
+                if mode != "read":
+                    raise Warning(
+                        _("Read only user can not done this operation..! (%s)")
+                        % self.env.user.name
+                    )
         return res
