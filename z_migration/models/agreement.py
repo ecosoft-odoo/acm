ed309d9c8907742bbec17c97737f2cc9ef0f9034
newtratip <tharathip.chaweewongphan@gmail.com>
2022-08-19 12:28:44 +0700

    import fields

--- z_migration/models/agreement.py
+++ z_migration/models/agreement.py
@@ -1,7 +1,7 @@
 # Copyright 2019 Ecosoft Co., Ltd (https://ecosoft.co.th/)
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html)
 
-from odoo import models, api
+from odoo import models, fields, api
 from lxml import etree
 
 
