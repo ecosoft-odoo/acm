ec845cf29a68502794dddf72076596c41d3319d2
newtratip <tharathip.chaweewongphan@gmail.com>
2023-01-19 18:37:35 +0700

    [FIX] acm_odoo_lite

---
+++ acm_odoo_lite/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2023 Ecosoft Co., Ltd (https://ecosoft.co.th/)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html)
+
+{
+    "name": "ACM :: Odoo Lite",
+    "version": "12.0.1.0.0",
+    "author": "Ecosoft",
+    "license": "AGPL-3",
+    "category": "Agreement",
+    "depends": ["acm"],
+    "data": [
+        "data/agreement_data.xml",
+        "data/agreement_sections_data.xml",
+        "data/agreement_clauses_data.xml",
+        "data/agreement_appendices_data.xml",
+        "views/account_analytic_account_views.xml",
+        "views/agreement_views.xml",
+    ]
+}
---
+++ acm_odoo_lite/models/account_analytic_account.py
@@ -0,0 +1,12 @@
+# Copyright 2023 Ecosoft Co., Ltd (https://ecosoft.co.th/)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html)
+
+from odoo import models, fields
+
+
+class AccountAnalyticAccount(models.Model):
+    _inherit = "account.analytic.account"
+
+    income_type_id = fields.Many2one(
+        required=False,
+    )
